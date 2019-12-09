aca4e68c3e03976e37d628957dbda553223c769c
Votizlov <SeriousFox@yandex.ru>
2019-12-09 23:28:32 +0300

    1 method

--- src/main/java/UI/Controller.java
+++ src/main/java/UI/Controller.java
@@ -1,11 +1,16 @@
 package UI;
 
+import MainLogic.Train;
+import MainLogic.TrainFormer;
 import javafx.event.ActionEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
+import java.util.LinkedList;
+
 public class Controller {
+    TrainFormer tf = new TrainFormer();
     public void saveFile(ActionEvent event) {
         FileChooser fc = new FileChooser();
         fc.showSaveDialog(new Stage());
@@ -14,5 +19,9 @@ public class Controller {
     public void loadFile(ActionEvent event) {
         FileChooser fc = new FileChooser();
         fc.showOpenDialog(new Stage());
+    }
+
+    public void form(ActionEvent event) {
+        LinkedList<Train> trains= tf.formTrains();
     }
 }
